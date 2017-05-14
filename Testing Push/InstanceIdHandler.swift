543d1c5a941a28eb7cbf8467b7dd480104e4895d
Noy Grisaru <noyngrisaru@gmail.com>
2017-05-14 13:36:34 +0300

    Swizzeling without fcm tutorial guidlines (just info.plist)

---
+++ Testing Push/InstanceIdHandler.swift
@@ -0,0 +1,23 @@
+import Foundation
+import Firebase
+
+class InstanceIdHandler {
+    
+    static let shared = InstanceIdHandler()
+    
+    private init() {
+        
+    }
+    
+    var token: String? {
+        return FIRInstanceID.instanceID().token()
+    }
+    
+    @objc func tokenWasRefreshed(_ notification: Notification) {
+        
+        if let refreshedToken = FIRInstanceID.instanceID().token() {
+            print("New refreshed token: \n\(refreshedToken)\n")
+        }
+        RemoteMessagingHandler.shared.connectToFcm()
+    }
+}
--- Testing Push/RemoteMessagingHandler.swift
+++ Testing Push/RemoteMessagingHandler.swift
@@ -1,6 +1,6 @@
 import Foundation
+import UserNotifications
 import Firebase
-
 
 class RemoteMessagingHandler: NSObject {
     
@@ -9,5 +9,103 @@ class RemoteMessagingHandler: NSObject {
     private override init() {
         
         super.init()
+    }
+    
+    func connectToFcm() {
+        
+        guard FIRInstanceID.instanceID().token() != nil else { return }
+        disconnectFromFcm()
+        FIRMessaging.messaging().connect { error in
+            if let error = error {
+                print("Unable to connect to FCM due to: \(error)")
+            } else {
+                print("Connected to FCM!")
+            }
+        }
+    }
+    
+    func disconnectFromFcm() {
+        
+        FIRMessaging.messaging().disconnect()
+        print("Disconnected from FCM!")
+    }
+    
+    func requestNotificationsPermission() {
+        
+        @available(iOS 10.0, *)
+        func requestAuthForUN() {
+            
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(options: authOptions, completionHandler: {_, _ in })
+        }
+        
+        func requestAuthForLocal() {
+            
+            let settings = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            UIApplication.shared.registerUserNotificationSettings(settings)
+        }
+        
+        if #available(iOS 10.0, *) {
+            requestAuthForUN()
+        } else {
+            requestAuthForLocal()
+        }
+    }
+    
+    func registerForRemoteMessages() {
+        
+        FIRMessaging.messaging().remoteMessageDelegate = self
+        UIApplication.shared.registerForRemoteNotifications()
+    }
+}
+
+
+//MARK: - Data Messages
+
+extension RemoteMessagingHandler: FIRMessagingDelegate {
+    
+    func applicationReceivedRemoteMessage(_ remoteMessage: FIRMessagingRemoteMessage) {
+        
+        guard let data = remoteMessage.appData as? [String: Any] else { return }
+        digestPushData(data)
+    }
+    
+    func digestPushData(_ data: [String: Any]) {
+        
+        guard let title = data["title"] as? String  else { return }
+        guard let body = data["body"] as? String else { return }
+        
+        if #available(iOS 10.0, *) {
+            showUNNotification(title: title, body: body)
+        } else {
+            showLocalNotification(title: title, body: body)
+        }
+    }
+    
+    @available(iOS 10.0, *)
+    private func showUNNotification(title: String, body: String) {
+        
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = body
+        content.sound = .default()
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 2, repeats: false)
+        let request = UNNotificationRequest(identifier: "com.test.not", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request) { error in
+            guard error != nil else {
+                print(error!)
+                return
+            }
+        }
+    }
+    
+    private func showLocalNotification(title: String, body: String) {
+        
+        let notification = UILocalNotification()
+        notification.alertTitle = title
+        notification.alertBody = body
+        notification.soundName = UILocalNotificationDefaultSoundName
+        UIApplication.shared.presentLocalNotificationNow(notification)
     }
 }
